30a27acc2e0761801c2e654851495e0be256b4ea
gmt2001 <garymt2001@hotmail.com>
2019-12-12 22:52:13 -0500

    Created IPlugin and PluginManager Added spellchecker config

---
+++ StreamActions-Bot/Plugin/IPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamActions.Plugin
+{
+    /// <summary>
+    /// An interface for a plugin that processes messages, commands, and other input
+    /// </summary>
+    public interface IPlugin
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Called when the plugin is disabled. Event delegates should be unregistered here
+        /// </summary>
+        public void Disabled();
+
+        /// <summary>
+        /// Called when the plugin is enabled. Event delegates should be registered here
+        /// </summary>
+        public void Enabled();
+
+        /// <summary>
+        /// Returns a basic, friendly description of the plugin
+        /// </summary>
+        /// <returns>A description of the plugin</returns>
+        public string GetPluginDescription();
+
+        /// <summary>
+        /// Returns a friendly name of the plugin
+        /// </summary>
+        /// <returns>The name of the plugin</returns>
+        public string GetPluginName();
+
+        /// <summary>
+        /// Returns the plugin version
+        /// </summary>
+        /// <returns>The plugin version</returns>
+        public string GetPluginVersion();
+
+        #endregion Public Methods
+    }
+}
---
+++ StreamActions-Bot/Plugin/PluginManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreamActions.Plugin
+{
+    /// <summary>
+    /// Loads and manages the state of plugins, as well as running the plugin event handlers
+    /// </summary>
+    public class PluginManager
+    {
+        #region Public Properties
+
+        public static PluginManager Instance { get { return _instance.Value; } }
+
+        #endregion Public Properties
+
+        #region Private Fields
+
+        private static readonly Lazy<PluginManager> _instance = new Lazy<PluginManager>(() => new PluginManager());
+
+        #endregion Private Fields
+
+        #region Private Constructors
+
+        private PluginManager()
+        {
+        }
+
+        #endregion Private Constructors
+    }
+}
