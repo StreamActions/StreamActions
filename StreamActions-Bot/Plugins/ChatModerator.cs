3b86715a93498ab5fabe3f85afd4ada64f1cb2e1
ScaniaTV <scaniatv@users.noreply.github.com>
2019-12-26 23:13:41 -0400

    - Added more moderation check methods

--- StreamActions-Bot/Plugins/ChatModerator.cs
+++ StreamActions-Bot/Plugins/ChatModerator.cs
@@ -16,7 +16,6 @@
 
 using StreamActions.Plugin;
 using System;
-using System.Collections.Generic;
 using System.Text;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
@@ -86,6 +85,106 @@ namespace StreamActions.Plugins
         }
 
         #endregion Public Methods
+
+        #region Filter check methods
+
+        /// <summary>
+        /// Method that checks if the message has a fake purge
+        /// </summary>
+        /// <param name="message">Message to be checked</param>
+        /// <returns>If the message has a fake purge.</returns>
+        private bool HasFakePurge(string message) =>
+            message.Equals("<message deleted>", StringComparison.OrdinalIgnoreCase) ||
+            message.Equals("message deleted by a moderator.", StringComparison.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Checks if the message has too many caps.
+        /// </summary>
+        /// <param name="message">The string message without any Twitch emotes.</param>
+        /// <param name="maximum">Maximum amount of caps allowed in a message.</param>
+        /// <returns>if we hit the caps limit.</returns>
+        private bool HasMaximumCaps(string message, int maximum)
+        {
+            bool hasMaximumCaps = false;
+            int count = 0;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (char.IsUpper(message[i]))
+                {
+                    count++;
+                    // Check if we hit the maximum allowed, no point in keeping going once we hit the maximum.
+                    if (count >= maximum)
+                    {
+                        hasMaximumCaps = true;
+                        break;
+                    }
+                }
+            }
+
+            return hasMaximumCaps;
+        }
+
+        /// <summary>
+        /// Method that checks if the message has too many emotes.
+        /// </summary>
+        /// <param name="message">Message to be checked.</param>
+        /// <param name="maximum">Maximum number of emotes allowed in a message.</param>
+        /// <returns>If the message has too many emotes.</returns>
+        private bool HasMaximumEmotes(EmoteSet emoteSet, int maximum) => emoteSet.Emotes.Count >= maximum;
+
+        /// <summary>
+        /// Method that checks if the message is lengthy.
+        /// </summary>
+        /// <param name="message">Message to be checked.</param>
+        /// <param name="maximum">Maximum length of message allowed.</param>
+        /// <returns>if the message is lenghty.</returns>
+        private bool HasMaximumMessageLength(string message, int maximum) => message.Length >= maximum;
+
+        /// <summary>
+        /// Method that checks if the message has too many symbols
+        /// </summary>
+        /// <param name="message">Message to be checked.</param>
+        /// <param name="maximum">Maximum allowed of non letters and digits.</param>
+        /// <returns>If we git the maximum symbols.</returns>
+        private bool HasMaximumSymbols(string message, int maximum)
+        {
+            bool hasMaximumSymbols = false;
+            int count = 0;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (!char.IsWhiteSpace(message[i]) && !char.IsLetterOrDigit(message[i]))
+                {
+                    count++;
+                    // Check if we hit the maximum allowed, no point in keeping going once we hit the maximum.
+                    if (count >= maximum)
+                    {
+                        hasMaximumSymbols = true;
+                        break;
+                    }
+                }
+            }
+
+            return hasMaximumSymbols;
+        }
+
+        /// <summary>
+        /// Method that removes Twitch emotes from a string.
+        /// </summary>
+        /// <param name="message">Main message.</param>
+        /// <param name="emoteSet">Emotes in the message.</param>
+        /// <returns>The message without any emotes.</returns>
+        private string RemoveEmotesFromMessage(string message, EmoteSet emoteSet)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // TODO: Implement logic to remove the emotes at specific indexes.
+
+            return sb.ToString();
+        }
+
+        #endregion Filter check methods
 
         #region Private Methods
 
@@ -246,34 +345,6 @@ namespace StreamActions.Plugins
             // TODO: Add filter check.
 
             return moderationResult;
-        }
-
-        /// <summary>
-        /// Checks if the message has too many caps.
-        /// </summary>
-        /// <param name="message">The string message without any Twitch emotes.</param>
-        /// <param name="maximum">Maximum amount of caps allowed in a message.</param>
-        /// <returns>if we hit the caps limit.</returns>
-        private bool HasMaximumCaps(string message, int maximum)
-        {
-            bool hasMaximumCaps = false;
-            int count = 0;
-
-            for (int i = 0; i < message.Length; i++)
-            {
-                if (char.IsUpper(message[i]))
-                {
-                    count++;
-                    // Check if we hit the maximum allowed, no point in keeping going once we hit the maximum.
-                    if (count >= maximum)
-                    {
-                        hasMaximumCaps = true;
-                        break;
-                    }
-                }
-            }
-
-            return hasMaximumCaps;
         }
 
         #endregion Private Methods
