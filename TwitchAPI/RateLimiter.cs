2e582fc2da1c0a48300c31982137500c95cf00de
gmt2001 <garymt2001@hotmail.com>
2021-12-20 21:47:22 -0500

    Start TwitchAPI Implement RateLimiter Update CodeMaid config

---
+++ TwitchAPI/RateLimiter.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Copyright © 2019-2021 StreamActions Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Net;
+
+namespace TwitchAPI
+{
+    /// <summary>
+    /// Handles Rate Limits.
+    /// </summary>
+    internal class RateLimiter
+    {
+        #region Internal Methods
+
+        /// <summary>
+        /// Parses response headers for Rate Limit information.
+        /// </summary>
+        /// <param name="headers">The response headers.</param>
+        internal void ParseHeaders(WebHeaderCollection headers)
+        {
+            lock (this._lock)
+            {
+                string? limitS = headers.Get("Ratelimit-Limit");
+
+                if (limitS is not null)
+                {
+                    _ = int.TryParse(limitS, out this._limit);
+                }
+
+                string? remainingS = headers.Get("Ratelimit-Remaining");
+
+                if (remainingS is not null)
+                {
+                    _ = int.TryParse(remainingS, out this._remaining);
+                }
+
+                string? resetS = headers.Get("Ratelimit-Reset");
+
+                if (resetS is not null)
+                {
+                    if (long.TryParse(resetS, out long reset))
+                    {
+                        this._reset = DateTime.UnixEpoch.AddSeconds(reset);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Waits until a token is available in the bucket, then aquires it.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> that can be awaited.</returns>
+        internal async Task WaitForRateLimit()
+        {
+            lock (this._lock)
+            {
+                this.RefillBucket();
+                if (this._remaining > 0)
+                {
+                    this._remaining--;
+                    return;
+                }
+            }
+
+            await Task.Delay(this.GetTimeUntilNextToken()).ConfigureAwait(false);
+            await this.WaitForRateLimit().ConfigureAwait(false);
+        }
+
+        #endregion Internal Methods
+
+        #region Private Fields
+
+        /// <summary>
+        /// The period over which the bucket normally refills from empty to full, in seconds.
+        /// </summary>
+        private static readonly int _period = 60;
+
+        /// <summary>
+        /// Lock object for aquring a token from the bucket.
+        /// </summary>
+        private readonly object _lock = new object();
+
+        /// <summary>
+        /// The last reported limit, when the bucket is full.
+        /// </summary>
+        private int _limit = 1;
+
+        /// <summary>
+        /// The remaining limit in the bucket.
+        /// </summary>
+        private int _remaining = 1;
+
+        /// <summary>
+        /// When the entire bucket will be full again if no more requests are made.
+        /// </summary>
+        private DateTime _reset = DateTime.UtcNow;
+
+        #endregion Private Fields
+
+        #region Private Methods
+
+        /// <summary>
+        /// Calculates when a new token should be available in the bucket.
+        /// </summary>
+        /// <returns>The time remaining until a new token is available in the bucket.</returns>
+        private TimeSpan GetTimeUntilNextToken() => this._reset.Subtract(this._reset.Subtract(TimeSpan.FromSeconds(_period))).Divide(this._limit);
+
+        /// <summary>
+        /// Refills the bucket.
+        /// </summary>
+        private void RefillBucket()
+        {
+            lock (this._lock)
+            {
+                int replenishedTokens = (int)Math.Floor(DateTime.UtcNow.Subtract(this._reset.Subtract(TimeSpan.FromSeconds(_period))).TotalSeconds * (this._limit / _period));
+                this._remaining += Math.Min(replenishedTokens, this._limit);
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ TwitchAPI/TwitchAPI.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * Copyright © 2019-2021 StreamActions Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace TwitchAPI
+{
+    /// <summary>
+    /// Handles validation of OAuth tokens and performs HTTP calls for the API.
+    /// </summary>
+    public class TwitchAPI
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="clientId">A valid Twitch App Client ID.</param>
+        /// <param name="oauth">A valid OAuth token made with via the <paramref name="clientId"/>.</param>
+        public TwitchAPI(string clientId, string oauth)
+        {
+            this._clientId = clientId;
+            this._oauth = oauth;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// The current list of scopes for the active OAuth token.
+        /// </summary>
+        public IReadOnlyList<string> Scopes => this._scopes.AsReadOnly();
+
+        #endregion Public Properties
+
+        #region Internal Enums
+
+        /// <summary>
+        /// HTTP Request Methods.
+        /// </summary>
+        internal enum Request_Method
+        {
+            /// <summary>
+            /// HTTP GET.
+            /// </summary>
+            GET,
+
+            /// <summary>
+            /// HTTP POST.
+            /// </summary>
+            POST,
+
+            /// <summary>
+            /// HTTP PUT.
+            /// </summary>
+            PUT,
+
+            /// <summary>
+            /// HTTP DELETE.
+            /// </summary>
+            DELETE
+        }
+
+        #endregion Internal Enums
+
+        #region Private Fields
+
+        /// <summary>
+        /// Helix base URL.
+        /// </summary>
+        private static readonly string _helix_base_url = "https://api.twitch.tv/helix/";
+
+        /// <summary>
+        /// User agent for requests.
+        /// </summary>
+        private static readonly string _user_agent = "StreamActions/TwitchAPI/1.0";
+
+        /// <summary>
+        /// Token validation URL.
+        /// </summary>
+        private static readonly string _validate_url = "https://id.twitch.tv/oauth2/validate";
+
+        /// <summary>
+        /// ClientID for TwitchAPI.
+        /// </summary>
+        private readonly string _clientId;
+
+        /// <summary>
+        /// OAuth for TwitchAPI.
+        /// </summary>
+        private readonly string _oauth;
+
+        /// <summary>
+        /// Backer for <see cref="Scopes"/>.
+        /// </summary>
+        private readonly List<string> _scopes = new List<string>();
+
+        #endregion Private Fields
+    }
+}
