fd0b6da406279ff60ea0c40f4d81e0d1fc37967c
gmt2001 <garymt2001@hotmail.com>
2022-02-20 22:24:51 -0500

    Implement Subscriptions endpoints

---
+++ StreamActions.Twitch/Api/Subscriptions/Subscription.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * Copyright © 2019-2022 StreamActions Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using StreamActions.Common;
+using StreamActions.Common.Exceptions;
+using StreamActions.Twitch.Api.Common;
+using System.Globalization;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+
+namespace StreamActions.Twitch.Api.Subscriptions
+{
+    /// <summary>
+    /// Sends and represents a response element for a list of subscribers.
+    /// </summary>
+    public record Subscription
+    {
+        /// <summary>
+        /// User ID of the broadcaster.
+        /// </summary>
+        [JsonPropertyName("broadcaster_id")]
+        public string? BroadcasterId { get; init; }
+
+        /// <summary>
+        /// Login of the broadcaster.
+        /// </summary>
+        [JsonPropertyName("broadcaster_login")]
+        public string? BroadcasterLogin { get; init; }
+
+        /// <summary>
+        /// Display name of the broadcaster.
+        /// </summary>
+        [JsonPropertyName("broadcaster_name")]
+        public string? BroadcasterName { get; init; }
+
+        /// <summary>
+        /// If the subscription was gifted, this is the user ID of the gifter. May not be defined for <see cref="CheckUserSubscription(TwitchSession, string, string)"/>.
+        /// </summary>
+        [JsonPropertyName("gifter_id")]
+        public string? GifterId { get; init; }
+
+        /// <summary>
+        /// If the subscription was gifted, this is the login of the gifter.
+        /// </summary>
+        [JsonPropertyName("gifter_login")]
+        public string? GifterLogin { get; init; }
+
+        /// <summary>
+        /// If the subscription was gifted, this is the display name of the gifter.
+        /// </summary>
+        [JsonPropertyName("gifter_name")]
+        public string? GifterName { get; init; }
+
+        /// <summary>
+        /// Is <c>true</c> if the subscription is a gift subscription.
+        /// </summary>
+        [JsonPropertyName("is_gift")]
+        public bool? IsGift { get; init; }
+
+        /// <summary>
+        /// Name of the subscription. May not be defined for <see cref="CheckUserSubscription(TwitchSession, string, string)"/>.
+        /// </summary>
+        [JsonPropertyName("plan_name")]
+        public string? PlanName { get; init; }
+
+        /// <summary>
+        /// Type of subscription (Tier 1, Tier 2, Tier 3).
+        /// </summary>
+        [JsonPropertyName("tier")]
+        public SubscriptionTier? Tier { get; init; }
+
+        /// <summary>
+        /// ID of the subscribed user. May not be defined for <see cref="CheckUserSubscription(TwitchSession, string, string)"/>.
+        /// </summary>
+        [JsonPropertyName("user_id")]
+        public string? UserId { get; init; }
+
+        /// <summary>
+        /// Display name of the subscribed user. May not be defined for <see cref="CheckUserSubscription(TwitchSession, string, string)"/>.
+        /// </summary>
+        [JsonPropertyName("user_login")]
+        public string? UserLogin { get; init; }
+
+        /// <summary>
+        /// Login of the subscribed user. May not be defined for <see cref="CheckUserSubscription(TwitchSession, string, string)"/>.
+        /// </summary>
+        [JsonPropertyName("user_name")]
+        public string? UserName { get; init; }
+
+        /// <summary>
+        /// Subscription tiers.
+        /// </summary>
+        public enum SubscriptionTier
+        {
+            /// <summary>
+            /// Default value. Not valid for use.
+            /// </summary>
+            None,
+            /// <summary>
+            /// Tier 1 or Prime subscription.
+            /// </summary>
+            Tier1 = 1000,
+            /// <summary>
+            /// Tier 2 subscription.
+            /// </summary>
+            Tier2 = 2000,
+            /// <summary>
+            /// Tier 3 subscription.
+            /// </summary>
+            Tier3 = 3000
+        }
+
+        /// <summary>
+        /// Gets a list of users that subscribe to the specified broadcaster.
+        /// </summary>
+        /// <param name="session">The <see cref="TwitchSession"/> to authorize the request.</param>
+        /// <param name="broadcasterId">User ID of the broadcaster. Must match the User ID in the Bearer token.</param>
+        /// <param name="userId">Filters the list to include only the specified subscribers. You may specify a maximum of 100 subscribers.</param>
+        /// <param name="after">Cursor for forward pagination: tells the server where to start fetching the next set of results in a multi-page response. This may only be used on queries without <paramref name="userId"/>. The cursor value specified here is from the pagination response field of a prior query.</param>
+        /// <param name="first">Maximum number of objects to return. Maximum: 100. Default: 20.</param>
+        /// <returns>A <see cref="SubscriptionResponse"/> with elements of type <see cref="Subscription"/> containing the response.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="session"/> is null; <paramref name="broadcasterId"/> is null, empty, or whitespace.</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="userId"/> is defined and has more than 100 elements; <paramref name="userId"/> and <paramref name="after"/> are both defined.</exception>
+        /// <exception cref="ScopeMissingException"><paramref name="session"/> contains a scope list, and <c>channel:read:subscriptions</c> is not present.</exception>
+        public static async Task<SubscriptionResponse?> GetBroadcasterSubscriptions(TwitchSession session, string broadcasterId, IEnumerable<string>? userId = null, string? after = null, int first = 20)
+        {
+            if (session is null)
+            {
+                throw new ArgumentNullException(nameof(session));
+            }
+
+            if (string.IsNullOrWhiteSpace(broadcasterId))
+            {
+                throw new ArgumentNullException(nameof(broadcasterId));
+            }
+
+            if (userId is not null && userId.Count() > 100)
+            {
+                throw new InvalidOperationException(nameof(userId) + " must have a count <= 100");
+            }
+
+            if (!session.Token?.HasScope("channel:read:subscriptions") ?? false)
+            {
+                throw new ScopeMissingException("channel:read:subscriptions");
+            }
+
+            first = Math.Clamp(first, 1, 100);
+
+            Dictionary<string, IEnumerable<string>> queryParams = new()
+            {
+                { "broadcaster_id", new List<string> { broadcasterId } },
+                { "first", new List<string> { first.ToString(CultureInfo.InvariantCulture) } }
+            };
+
+            if (userId is not null && userId.Any())
+            {
+                List<string> userIds = new();
+                foreach (string pollId in userId)
+                {
+                    if (!string.IsNullOrWhiteSpace(pollId))
+                    {
+                        userIds.Add(pollId);
+                    }
+                }
+
+                if (userIds.Any())
+                {
+                    if (!string.IsNullOrWhiteSpace(after))
+                    {
+                        throw new InvalidOperationException(nameof(userId) + " can not be used at the same time as " + nameof(after));
+                    }
+
+                    queryParams.Add("user_id", userIds);
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(after))
+            {
+                queryParams.Add("after", new List<string> { after });
+            }
+
+            Uri uri = Util.BuildUri(new("/subscriptions"), queryParams);
+            HttpResponseMessage response = await TwitchApi.PerformHttpRequest(HttpMethod.Get, uri, session).ConfigureAwait(false);
+            return await response.Content.ReadFromJsonAsync<SubscriptionResponse?>().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Checks if a specific user (<paramref name="userId"/>) is subscribed to a specific channel (<paramref name="broadcasterId"/>).
+        /// </summary>
+        /// <param name="session">The <see cref="TwitchSession"/> to authorize the request.</param>
+        /// <param name="broadcasterId">User ID of the broadcaster. Must match the User ID in the Bearer token.</param>
+        /// <param name="userId">User ID of a Twitch viewer.</param>
+        /// <returns>A <see cref="SubscriptionResponse"/> with elements of type <see cref="Subscription"/> containing the response. The response contains a <see cref="TwitchResponse.Status"/> of <see cref="System.Net.HttpStatusCode.NotFound"/> if the <paramref name="userId"/> is not subscribed to <paramref name="broadcasterId"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="session"/> is null; <paramref name="broadcasterId"/> or <paramref name="userId"/> is null, empty, or whitespace.</exception>
+        /// <exception cref="ScopeMissingException"><paramref name="session"/> contains a scope list, and <c>channel:read:subscriptions</c> is not present.</exception>
+        public static async Task<SubscriptionResponse?> CheckUserSubscription(TwitchSession session, string broadcasterId, string userId)
+        {
+            if (session is null)
+            {
+                throw new ArgumentNullException(nameof(session));
+            }
+
+            if (string.IsNullOrWhiteSpace(broadcasterId))
+            {
+                throw new ArgumentNullException(nameof(broadcasterId));
+            }
+
+            if (string.IsNullOrWhiteSpace(userId))
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+
+            if (!session.Token?.HasScope("channel:read:subscriptions") ?? false)
+            {
+                throw new ScopeMissingException("channel:read:subscriptions");
+            }
+
+            Dictionary<string, IEnumerable<string>> queryParams = new()
+            {
+                { "broadcaster_id", new List<string> { broadcasterId } },
+                { "user_id", new List<string> { userId } }
+            };
+
+            Uri uri = Util.BuildUri(new("/subscriptions/user"), queryParams);
+            HttpResponseMessage response = await TwitchApi.PerformHttpRequest(HttpMethod.Get, uri, session).ConfigureAwait(false);
+            return await response.Content.ReadFromJsonAsync<SubscriptionResponse?>().ConfigureAwait(false);
+        }
+    }
+}
---
+++ StreamActions.Twitch/Api/Subscriptions/SubscriptionResponse.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copyright © 2019-2022 StreamActions Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using StreamActions.Twitch.Api.Common;
+using System.Text.Json.Serialization;
+
+namespace StreamActions.Twitch.Api.Subscriptions
+{
+    /// <summary>
+    /// A <see cref="ResponseData{TDataType}"/> with the subscription <see cref="Points"/> field.
+    /// </summary>
+    public record SubscriptionResponse : ResponseData<Subscription>
+    {
+        /// <summary>
+        /// The current number of subscriber points earned by this broadcaster.
+        /// </summary>
+        /// <remarks>
+        /// Points are based on the subscription tier of each user that subscribes to this broadcaster.
+        /// For example, a Tier 1 subscription is worth 1 point, Tier 2 is worth 2 points, and Tier 3 is worth 6 points.
+        /// The number of points determines the number of emote slots that are unlocked for the broadcaster.
+        /// </remarks>
+        [JsonPropertyName("points")]
+        public int? Points { get; init; }
+    }
+}
