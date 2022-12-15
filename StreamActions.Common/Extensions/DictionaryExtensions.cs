72a0d0f847716c100fab2b1bd45e793f4c273362
gmt2001 <garymt2001@hotmail.com>
2022-12-15 12:05:56 -0500

    Add DictionaryExtensions with AsReadOnly extension

---
+++ StreamActions.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * This file is part of StreamActions.
+ * Copyright © 2019-2022 StreamActions Team (streamactions.github.io)
+ *
+ * StreamActions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StreamActions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with StreamActions.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.ObjectModel;
+
+namespace StreamActions.Common.Extensions;
+
+/// <summary>
+/// Contains extension members to <see cref="IDictionary{TKey, TValue}"/>.
+/// </summary>
+public static class DictionaryExtensions
+{
+    #region Public Methods
+
+    /// <summary>
+    /// Returns <paramref name="dictionary"/> wrapped in a <see cref="IReadOnlyDictionary{TKey, TValue}"/>.
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    /// <param name="dictionary">The dictionary to wrap.</param>
+    /// <returns>A <see cref="IReadOnlyDictionary{TKey, TValue}"/> that is wrapping <paramref name="dictionary"/>.</returns>
+    public static IReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) where TKey : notnull => new ReadOnlyDictionary<TKey, TValue>(dictionary);
+
+    #endregion Public Methods
+}
