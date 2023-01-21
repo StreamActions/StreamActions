410ebb97d1af853568f20b3d31858c13b6f7f149
gmt2001 <garymt2001@hotmail.com>
2023-01-21 07:42:59 -0500

    Refactor Commercial/StartCommercialParameters to standards

--- StreamActions.Twitch/Api/Ads/Commercial.cs
+++ StreamActions.Twitch/Api/Ads/Commercial.cs
@@ -35,7 +35,7 @@ public sealed record Commercial
     /// Length of the triggered commercial.
     /// </summary>
     [JsonPropertyName("length")]
-    public int? Length { get; init; }
+    public CommercialLength? Length { get; init; }
 
     /// <summary>
     /// Seconds until the next commercial can be served on this channel.
@@ -56,14 +56,57 @@ public sealed record Commercial
     public string? Message { get; init; }
 
     /// <summary>
-    /// Starts a commercial on a specified channel.
+    /// Valid lengths for a commercial break.
+    /// </summary>
+    public enum CommercialLength
+    {
+        /// <summary>
+        /// Default value. Not valid for use.
+        /// </summary>
+        None,
+        /// <summary>
+        /// 30 seconds.
+        /// </summary>
+        Thirty = 30,
+        /// <summary>
+        /// 1 minute.
+        /// </summary>
+        Sixty = 60,
+        /// <summary>
+        /// 1 minute, 30 seconds.
+        /// </summary>
+        Ninety = 90,
+        /// <summary>
+        /// 2 minutes.
+        /// </summary>
+        OneTwenty = 120,
+        /// <summary>
+        /// 2 minutes, 30 seconds.
+        /// </summary>
+        OneFifty = 150,
+        /// <summary>
+        /// 3 minutes.
+        /// </summary>
+        OneEighty = 180
+    }
+
+    /// <summary>
+    /// Starts a commercial on the specified channel.
     /// </summary>
     /// <param name="session">The <see cref="TwitchSession"/> to authorize the request.</param>
     /// <param name="parameters">The <see cref="StartCommercialParameters"/> with the request parameters.</param>
     /// <returns>A <see cref="ResponseData{TDataType}"/> with elements of type <see cref="Commercial"/> containing the response.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="session"/> or <paramref name="parameters"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><see cref="StartCommercialParameters.BroadcasterId"/> is <see langword="null"/>, empty, or whitespace; <see cref="StartCommercialParameters.Length"/> is <see langword="null"/> or <see cref="CommercialLength.None"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><see cref="StartCommercialParameters.Length"/> is not a valid value of <see cref="CommercialLength"/>.</exception>
     /// <exception cref="InvalidOperationException"><see cref="TwitchSession.Token"/> is <see langword="null"/>; <see cref="TwitchToken.OAuth"/> is <see langword="null"/>, empty, or whitespace.</exception>
     /// <exception cref="TwitchScopeMissingException"><paramref name="session"/> does not have the scope <see cref="Scope.ChannelEditCommercial"/>.</exception>
+    /// <remarks>
+    /// <para>
+    /// Only partners and affiliates may run commercials and they must be streaming live at the time.
+    /// </para>
+    /// </remarks>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "Intentional")]
     public static async Task<ResponseData<Commercial>?> StartCommercial(TwitchSession session, StartCommercialParameters parameters)
     {
         if (session is null)
@@ -74,6 +117,21 @@ public sealed record Commercial
         if (parameters is null)
         {
             throw new ArgumentNullException(nameof(parameters)).Log(TwitchApi.GetLogger());
+        }
+
+        if (string.IsNullOrWhiteSpace(parameters.BroadcasterId))
+        {
+            throw new ArgumentNullException(nameof(parameters.BroadcasterId)).Log(TwitchApi.GetLogger());
+        }
+
+        if (!parameters.Length.HasValue || parameters.Length.Value == CommercialLength.None)
+        {
+            throw new ArgumentNullException(nameof(parameters.Length)).Log(TwitchApi.GetLogger());
+        }
+
+        if (!Enum.IsDefined(parameters.Length.Value))
+        {
+            throw new ArgumentOutOfRangeException(nameof(parameters.Length), parameters.Length.Value, "not a valid commercial length").Log(TwitchApi.GetLogger());
         }
 
         session.RequireToken(Scope.ChannelEditCommercial);
--- StreamActions.Twitch/Api/Ads/StartCommercialParameters.cs
+++ StreamActions.Twitch/Api/Ads/StartCommercialParameters.cs
@@ -16,7 +16,6 @@
  * along with StreamActions.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-using StreamActions.Common.Logger;
 using System.Text.Json.Serialization;
 
 namespace StreamActions.Twitch.Api.Ads;
@@ -27,71 +26,14 @@ namespace StreamActions.Twitch.Api.Ads;
 public sealed record StartCommercialParameters
 {
     /// <summary>
-    /// Constructor.
-    /// </summary>
-    /// <param name="broadcasterId">ID of the channel requesting a commercial.</param>
-    /// <param name="length">Desired length of the commercial in seconds.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="broadcasterId"/> is <see langword="null"/>, empty, or whitespace; <paramref name="length"/> is <see cref="CommercialLength.None"/></exception>
-    public StartCommercialParameters(string broadcasterId, CommercialLength length)
-    {
-        if (string.IsNullOrWhiteSpace(broadcasterId))
-        {
-            throw new ArgumentNullException(nameof(broadcasterId)).Log(TwitchApi.GetLogger());
-        }
-
-        if (length == CommercialLength.None)
-        {
-            throw new ArgumentNullException(nameof(length)).Log(TwitchApi.GetLogger());
-        }
-
-        this.BroadcasterId = broadcasterId;
-        this.Length = (int)length;
-    }
-
-    /// <summary>
     /// ID of the channel requesting a commercial.
     /// </summary>
     [JsonPropertyName("broadcaster_id")]
-    public string BroadcasterId { get; private init; }
+    public string? BroadcasterId { get; init; }
 
     /// <summary>
     /// Desired length of the commercial in seconds.
     /// </summary>
     [JsonPropertyName("length")]
-    public int Length { get; private init; }
-
-    /// <summary>
-    /// Valid lengths for a commercial break.
-    /// </summary>
-    public enum CommercialLength
-    {
-        /// <summary>
-        /// Default value. Not valid for use.
-        /// </summary>
-        None,
-        /// <summary>
-        /// 30 seconds.
-        /// </summary>
-        Thirty = 30,
-        /// <summary>
-        /// 1 minute.
-        /// </summary>
-        Sixty = 60,
-        /// <summary>
-        /// 1 minute, 30 seconds.
-        /// </summary>
-        Ninety = 90,
-        /// <summary>
-        /// 2 minutes.
-        /// </summary>
-        OneTwenty = 120,
-        /// <summary>
-        /// 2 minutes, 30 seconds.
-        /// </summary>
-        OneFifty = 150,
-        /// <summary>
-        /// 3 minutes.
-        /// </summary>
-        OneEighty = 180
-    }
+    public Commercial.CommercialLength? Length { get; init; }
 }
