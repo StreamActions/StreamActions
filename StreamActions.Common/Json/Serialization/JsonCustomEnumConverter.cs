c8f2d191df67a50f4ab1d74e3f51669f015c447e
gmt2001 <garymt2001@hotmail.com>
2023-05-03 06:02:54 -0400

    Add ability to use JsonCustomEnumConverter standalone

--- StreamActions.Common/Json/Serialization/JsonCustomEnumConverter.cs
+++ StreamActions.Common/Json/Serialization/JsonCustomEnumConverter.cs
@@ -30,17 +30,28 @@ public sealed class JsonCustomEnumConverter<T> : JsonConverter<T> where T : stru
     #region Public Methods
 
     /// <inheritdoc/>
-    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        string val = reader.GetString()!;
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => this.Convert(reader.GetString()!);
 
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) => writer?.WriteStringValue(this.Convert(value));
+
+    /// <summary>
+    /// Converts the given string into an enum value.
+    /// </summary>
+    /// <remarks>
+    /// The string comparison is performed using <see cref="StringComparison.InvariantCultureIgnoreCase"/>.
+    /// </remarks>
+    /// <param name="value">The string value to convert.</param>
+    /// <returns>The enum value of type <typeparamref name="T"/> that has a matching <see cref="JsonCustomEnumAttribute"/>; <see langword="default"/> if there is no match.</returns>
+    public T Convert(string value)
+    {
         foreach (string eval in Enum.GetNames<T>())
         {
             MemberInfo? memberInfo = typeof(T).GetMember(eval).FirstOrDefault();
             if (memberInfo is not null)
             {
                 Attribute? attribute = Attribute.GetCustomAttribute(memberInfo, typeof(JsonCustomEnumAttribute));
-                if (attribute is not null && ((attribute as JsonCustomEnumAttribute)?.Value.Equals(val, StringComparison.InvariantCultureIgnoreCase) ?? false))
+                if (attribute is not null && ((attribute as JsonCustomEnumAttribute)?.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase) ?? false))
                 {
                     return Enum.Parse<T>(eval);
                 }
@@ -50,8 +61,12 @@ public sealed class JsonCustomEnumConverter<T> : JsonConverter<T> where T : stru
         return default;
     }
 
-    /// <inheritdoc/>
-    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    /// <summary>
+    /// Converts the given enum value into a string.
+    /// </summary>
+    /// <param name="value">The enum value to convert.</param>
+    /// <returns>The string value defined in the <see cref="JsonCustomEnumAttribute"/> applied to the enum value; <see cref="Enum.GetName{TEnum}(TEnum)"/> if the attribute is not present.</returns>
+    public string? Convert(T value)
     {
         MemberInfo? memberInfo = typeof(T).GetMember(Enum.GetName(typeof(T), value) ?? "__").FirstOrDefault();
         if (memberInfo is not null)
@@ -59,11 +74,11 @@ public sealed class JsonCustomEnumConverter<T> : JsonConverter<T> where T : stru
             Attribute? attribute = Attribute.GetCustomAttribute(memberInfo, typeof(JsonCustomEnumAttribute));
             if (attribute is not null)
             {
-                writer?.WriteStringValue((attribute as JsonCustomEnumAttribute)?.Value);
+                return (attribute as JsonCustomEnumAttribute)?.Value;
             }
         }
 
-        writer?.WriteStringValue(Enum.GetName(value));
+        return Enum.GetName(value);
     }
 
     #endregion Public Methods
