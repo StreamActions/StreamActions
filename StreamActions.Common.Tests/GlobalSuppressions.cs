1cb583561e09251bbf13f2a3f0e56790a53f4e22
gmt2001 <garymt2001@hotmail.com>
2023-05-14 15:44:13 -0400

    Add unit tests for Util.BuildUri Add Traits to group tests Add global suppression for underscores in member names for testing assembly

---
+++ StreamActions.Common.Tests/GlobalSuppressions.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This file is part of StreamActions.
+ * Copyright © 2019-2023 StreamActions Team (streamactions.github.io)
+ *
+ * StreamActions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StreamActions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with StreamActions.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "xUnit Test Case Naming", Scope = "module")]
--- StreamActions.Common.Tests/UtilTests.cs
+++ StreamActions.Common.Tests/UtilTests.cs
@@ -16,6 +16,8 @@
  * along with StreamActions.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System.Collections.Specialized;
+
 namespace StreamActions.Common.Tests;
 
 /// <summary>
@@ -23,12 +25,8 @@ namespace StreamActions.Common.Tests;
 /// </summary>
 public class UtilTests
 {
-    /// <summary>
-    /// Unit tests for <see cref="Util.IsValidHexColor(string)"/>.
-    /// </summary>
-    /// <param name="hexColor">The input string to test.</param>
-    /// <param name="expected">The expected result.</param>
     [Theory]
+    [Trait("Member", "IsValidHexColor")]
     [InlineData("#000000", true)]
     [InlineData("#123456", true)]
     [InlineData("#ABCDEF", true)]
@@ -49,5 +47,231 @@ public class UtilTests
     [InlineData("#FF5733 is at the beginning of this line.", false)]
     [InlineData("This line ends with #FF5733.", false)]
     [InlineData(null, false)]
-    public void IsValidHexColorTest(string hexColor, bool expected) => Util.IsValidHexColor(hexColor).Should().Be(expected);
+    public void IsValidHexColor_ReturnsCorrectBoolean(string hexColor, bool expected) => Util.IsValidHexColor(hexColor).Should().Be(expected);
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithNullBaseUri_ThrowsArgumentNullException()
+    {
+        // Arrange
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        Uri baseUri = null;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+        // Act
+#pragma warning disable CS8604 // Possible null reference argument.
+        Action act = () => Util.BuildUri(baseUri);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        // Assert
+        _ = act.Should().Throw<ArgumentNullException>()
+            .And.ParamName.Should().Be("baseUri");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithQueryParams_EscapesAndAppendsQueryParams()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1=value1&key2=value2");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithFragmentParams_EscapesAndAppendsFragmentParams()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection fragmentParams = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, null, fragmentParams);
+
+        // Assert
+        _ = result.Fragment.Should().Be("#key1=value1&key2=value2");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithNullValueInQueryParams_AppendsKeyWithoutValue()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+            {
+                { "key1", null }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithUrlEncodingInQueryParams_EncodesValues()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+            {
+                { "key1", "value 1" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1=value%201");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithSingleQueryParam_AppendsSingleQueryParam()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+            {
+                { "key1", "value1" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1=value1");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithBaseUriOnly_ReturnsBaseUri()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+
+        // Act
+        Uri result = Util.BuildUri(baseUri);
+
+        // Assert
+        _ = result.Should().Be(baseUri);
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithAllParameters_ReturnsExpectedResult()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+             {
+                 { "key1", "value 1" },
+                 { "key2", null }
+             };
+        NameValueCollection fragmentParams = new()
+             {
+                 { "key3", "value3" },
+                 { "key4", null }
+             };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams, fragmentParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1=value%201&key2");
+        _ = result.Fragment.Should().Be("#key3=value3&key4");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithBaseUriContainingQueryParam_AppendsAdditionalQueryParams()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com?existingKey=existingValue");
+        NameValueCollection queryParams = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?existingKey=existingValue&key1=value1&key2=value2");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithBaseUriContainingFragmentParam_AppendsAdditionalFragmentParams()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com#existingKey=existingValue");
+        NameValueCollection fragmentParams = new()
+            {
+                { "key1", "value1" },
+                { "key2", "value2" }
+            };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, null, fragmentParams);
+
+        // Assert
+        _ = result.Fragment.Should().Be("#existingKey=existingValue&key1=value1&key2=value2");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithMultipleValuesForKeyInQueryParams_AppendsAllValuesForKey()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection queryParams = new()
+        {
+            { "key1", "value1" },
+            { "key1", "value2" }
+        };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, queryParams);
+
+        // Assert
+        _ = result.Query.Should().Be("?key1=value1&key1=value2");
+    }
+
+    [Fact]
+    [Trait("Member", "BuildUri")]
+    public void BuildUri_WithMultipleValuesForKeyInFragmentParams_AppendsAllValuesForKey()
+    {
+        // Arrange
+        Uri baseUri = new("https://example.com");
+        NameValueCollection fragmentParams = new()
+        {
+            { "key1", "value1" },
+            { "key1", "value2" }
+        };
+
+        // Act
+        Uri result = Util.BuildUri(baseUri, null, fragmentParams);
+
+        // Assert
+        _ = result.Fragment.Should().Be("#key1=value1&key1=value2");
+    }
 }
