300e2ecadcbf61b6901d60fdfe5043b8effea013
gmt2001 <garymt2001@hotmail.com>
2023-12-18 02:28:38 -0500

    Use primary constructor

--- StreamActions.Common/Attributes/ETagAttribute.cs
+++ StreamActions.Common/Attributes/ETagAttribute.cs
@@ -23,37 +23,25 @@ namespace StreamActions.Common.Attributes;
 /// <summary>
 /// Indicates the URI and ETag associated with a <see cref="Interfaces.IComponent"/>, allowing <c>Diff.py</c> to automatically detect when the source documentation has changed.
 /// </summary>
+/// <param name="friendlyName">The friendly name of the documentation, to be used when opening issues programmatically.</param>
+/// <param name="issueTags">Tags to be used when opening issues programmatically.</param>
+/// <param name="uri">The URI of the source documentation for this component.</param>
+/// <param name="eTag">The ETag of the latest version of the documentation that this component conforms to.</param>
+/// <param name="timestamp">The timestamp when the <paramref name="eTag"/> was updated.</param>
+/// <param name="stripParameters">Parameters for <c>StripData.py</c>.</param>
+/// <remarks>
+/// <para>
+/// Main RegEx (Python3): <code>r"\[ETag\(\s*\"(?P<friendlyname>[^\"]+)\",\s*[^{\[]+[{\[](?P<issuetags>.*)[}\]],\s*\"(?P<url>[^\"]+)\",\s*\"(?P<hash>[^\"]+)\",\s*\"(?P<date>[^\"]+)\",\s*\"(?P<parser>[^\"]+)\"\)\]"s</code>
+/// </para>
+/// <para>
+/// Array RegEx (Python3): <code>r"(\"(?P<param>([^\"]|\\\")+)\"(,|$))"gs</code>
+/// </para>
+/// </remarks>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public sealed class ETagAttribute : Attribute
+public sealed class ETagAttribute(string friendlyName, string[] issueTags, string uri, string eTag, string timestamp, string parser) : Attribute
 {
-    #region Public Constructors
 
-    /// <summary>
-    /// Attribute Constructor.
-    /// </summary>
-    /// <param name="friendlyName">The friendly name of the documentation, to be used when opening issues programmatically.</param>
-    /// <param name="issueTags">Tags to be used when opening issues programmatically.</param>
-    /// <param name="uri">The URI of the source documentation for this component.</param>
-    /// <param name="eTag">The ETag of the latest version of the documentation that this component conforms to.</param>
-    /// <param name="timestamp">The timestamp when the <paramref name="eTag"/> was updated.</param>
-    /// <param name="stripParameters">Parameters for <c>StripData.py</c>.</param>
-    /// <remarks>
-    /// <para>
-    /// Main RegEx (Python3): <code>r"\[ETag\(\s*\"(?P<friendlyname>[^\"]+)\",\s*[^{\[]+[{\[](?P<issuetags>.*)[}\]],\s*\"(?P<url>[^\"]+)\",\s*\"(?P<hash>[^\"]+)\",\s*\"(?P<date>[^\"]+)\",\s*\"(?P<parser>[^\"]+)\"\)\]"s</code>
-    /// </para>
-    /// <para>
-    /// Array RegEx (Python3): <code>r"(\"(?P<param>([^\"]|\\\")+)\"(,|$))"gs</code>
-    /// </para>
-    /// </remarks>
-    public ETagAttribute(string friendlyName, string[] issueTags, string uri, string eTag, string timestamp, string parser)
-    {
-        this.FriendlyName = friendlyName;
-        this.IssueTags = issueTags;
-        this.Uri = uri;
-        this.ETag = eTag;
-        this.Timestamp = DateTime.Parse(timestamp, CultureInfo.InvariantCulture);
-        this.Parser = parser;
-    }
+    #region Public Constructors
 
     #endregion Public Constructors
 
@@ -62,32 +50,32 @@ public sealed class ETagAttribute : Attribute
     /// <summary>
     /// The ETag of the latest version of the documentation that this component conforms to.
     /// </summary>
-    public string ETag { get; init; }
+    public string ETag { get; init; } = eTag;
 
     /// <summary>
     /// The friendly name of the documentation, to be used when opening issues programmatically.
     /// </summary>
-    public string FriendlyName { get; init; }
+    public string FriendlyName { get; init; } = friendlyName;
 
     /// <summary>
     /// Tags to be used when opening issues programmatically.
     /// </summary>
-    public IReadOnlyCollection<string> IssueTags { get; init; }
+    public IReadOnlyCollection<string> IssueTags { get; init; } = issueTags;
 
     /// <summary>
     /// The parser module to use
     /// </summary>
-    public string Parser { get; init; }
+    public string Parser { get; init; } = parser;
 
     /// <summary>
     /// The timestamp when the <see cref="ETag"/> was updated.
     /// </summary>
-    public DateTime Timestamp { get; init; }
+    public DateTime Timestamp { get; init; } = DateTime.Parse(timestamp, CultureInfo.InvariantCulture);
 
     /// <summary>
     /// The URI of the source documentation for this component.
     /// </summary>
-    public string Uri { get; init; }
+    public string Uri { get; init; } = uri;
 
     #endregion Public Properties
 }
