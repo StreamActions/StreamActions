607b14615ddc8c080da280cb3eab484fd834e15b
gmt2001 <garymt2001@hotmail.com>
2024-06-22 23:49:46 -0400

    Move glob to parameter Update parameter names

--- Diff/ETagFinder.py
+++ Diff/ETagFinder.py
@@ -30,25 +30,27 @@ import re
 etagpattern = re.compile(r"(?s)\[ETag\(\s*\"(?P<friendlyname>[^\"]+)\"\s*,\s*(?P<issue>[0-9]+)\s*,\s*\"(?P<url>[^\"]+)\"\s*,\s*\"(?P<hash>[^\"]+)\"\s*,\s*\"(?P<date>[^\"]+)\"\s*,\s*\"(?P<parser>[^\"]+)\"\s*,\s*[{\[](?P<parameters>.*?)[}\]]\s*\)\]")
 parampattern = re.compile(r"(?s)(\"(?P<param>([^\"]|\\\")+)\"(,|$))")
 
-def testfolder(folder: str | Path):
-    """Tests if any files in the folder contain an ETag attribute and returns the results
+def testfolder(folder: str | Path, glob: str = "*.cs"):
+    """Tests if any files in the folder, or sub-folders, matching the glob contain an ETag attribute and returns the results
 
     folder: The folder to check
 
-    returns: A dict of files where at least 1 ETag attribute was found. The key is the filename relative to ETagFinder.py, the value is the output of testfile(file)
+    glob: The glob to use for picking files to check
+
+    returns: A dict of files where at least 1 ETag attribute was found. The key is the file path relative to ETagFinder.py, the value is the output of testfile(filePath)
     """
     ret = {}
     execpath = os.path.dirname(os.path.realpath(__file__))
-    for file in Path(folder).rglob("*.cs"):
+    for file in Path(folder).rglob(glob):
         data = testfile(file)
         if data is not None:
             ret[str(file.absolute().relative_to(execpath, walk_up=True))] = data
     return ret
 
-def testfile(file: str | Path):
+def testfile(filePath: str | Path):
     """Tests if the file contains an ETag attribute and returns the result
 
-    file: The file to check
+    filePath: The file to check
 
     returns: `None` if no matches are found; otherwise a list containing dicts as described below
 
@@ -69,8 +71,8 @@ def testfile(file: str | Path):
         ...
     ]
     """
-    with open(file, "r", encoding="utf8") as csfile:
-        lines = csfile.read()
+    with open(filePath, "r", encoding="utf8") as file:
+        lines = file.read()
     m = etagpattern.search(lines)
     if m is None:
         return None
@@ -91,7 +93,7 @@ def testfile(file: str | Path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find ETag attributes in a fodler structure and return their parameters")
     parser.add_argument("--folder", action="store", help="The folder to search")
-    parser.add_argument("--out", action="store", help="Output as JSON to the specified file instead of STDOUT")
+    parser.add_argument("--out", action="store", help="Output to the specified file instead of STDOUT")
     args = parser.parse_args()
     ret = testfolder(args.folder)
     if args.out == None:
