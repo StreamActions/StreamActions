fe2ae5d87cfa4cc3d5a1ce869b8ad2a68fa9c4d3
gmt2001 <garymt2001@hotmail.com>
2024-12-29 23:08:40 -0500

    Update Copyright

--- StreamActions/Pages/Error.cshtml.cs
+++ StreamActions/Pages/Error.cshtml.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of StreamActions.
- * Copyright © 2019-2024 StreamActions Team (streamactions.github.io)
+ * Copyright © 2019-2025 StreamActions Team (streamactions.github.io)
  *
  * StreamActions is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
