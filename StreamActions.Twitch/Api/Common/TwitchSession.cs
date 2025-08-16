01f0c515dd93d85621c22ff35965af3e31597459
gmt2001 <garymt2001@hotmail.com>
2025-08-16 11:15:27 -0400

    Replace RequireToken with methods specific to token type

--- StreamActions.Twitch/Api/Common/TwitchSession.cs
+++ StreamActions.Twitch/Api/Common/TwitchSession.cs
@@ -16,6 +16,7 @@
  * along with StreamActions.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using StreamActions.Common.Exceptions;
 using StreamActions.Common.Limiters;
 using StreamActions.Common.Logger;
 using StreamActions.Twitch.Exceptions;
@@ -28,10 +29,16 @@ namespace StreamActions.Twitch.Api.Common;
 /// </summary>
 public sealed record TwitchSession : IDisposable
 {
+    #region Public Fields
+
     /// <summary>
     /// A default TwitchSession with an empty token, for requests that do not need one.
     /// </summary>
     public static readonly TwitchSession Empty = new() { Token = TwitchToken.Empty };
+
+    #endregion Public Fields
+
+    #region Public Properties
 
     /// <summary>
     /// Rate Limiter for TwitchAPI.
@@ -48,15 +55,9 @@ public sealed record TwitchSession : IDisposable
         internal set => this._token = value ?? throw new ArgumentNullException(nameof(value)).Log(TwitchApi.GetLogger());
     }
 
-    /// <summary>
-    /// Used as a locking object by <see cref="TwitchApi.PerformHttpRequest(HttpMethod, Uri, TwitchSession, HttpContent?, bool)"/> to prevent multiple simultaneous or back-to-back OAuth Refreshes.
-    /// </summary>
-    internal Mutex _refreshLock = new();
+    #endregion Public Properties
 
-    /// <summary>
-    /// Backer for <see cref="Token"/>.
-    /// </summary>
-    private TwitchToken? _token;
+    #region Public Methods
 
     /// <summary>
     /// <inheritdoc/>
@@ -64,14 +65,11 @@ public sealed record TwitchSession : IDisposable
     public void Dispose() => this._refreshLock.Close();
 
     /// <summary>
-    /// Checks if a token is present and, optionally, a scope.
+    /// Checks if an app token is present.
     /// </summary>
-    /// <param name="scope">The scope to check for.</param>
-    /// <param name="alternateScope">An alternate scope to check for.</param>
-    /// <param name="retIfNull">If <see cref="TwitchToken.Scopes"/> is <see langword="null"/> and this is <see langword="true"/>, <see cref="TwitchScopeMissingException"/> is not thrown.</param>
     /// <exception cref="InvalidOperationException"><see cref="Token"/> is <see langword="null"/>; <see cref="TwitchToken.OAuth"/> is <see langword="null"/>, empty, or whitespace.</exception>
-    /// <exception cref="TwitchScopeMissingException"><see cref="TwitchToken.Scopes"/> does not contain <paramref name="scope"/> or <paramref name="alternateScope"/>.</exception>
-    public void RequireToken(Scope? scope = null, Scope? alternateScope = null, bool retIfNull = false)
+    /// <exception cref="TokenTypeException"><see cref="TwitchToken.Type"/> is not <see cref="TwitchToken.TokenType.App"/>.</exception>
+    public void RequireAppToken()
     {
         if (this.Token is null)
         {
@@ -83,20 +81,102 @@ public sealed record TwitchSession : IDisposable
             throw new InvalidOperationException(nameof(this.Token.OAuth) + " is null, empty, or whitespace.").Log(TwitchApi.GetLogger());
         }
 
-        bool hasScope = false;
-        if (scope is not null)
+        if (this.Token.Type is not TwitchToken.TokenType.App)
         {
-            hasScope = this.Token.HasScope(scope, alternateScope is null && retIfNull);
-        }
-
-        if (!hasScope && alternateScope is not null)
-        {
-            hasScope = this.Token.HasScope(alternateScope, retIfNull);
-        }
-
-        if (!hasScope)
-        {
-            throw new TwitchScopeMissingException(scope).Log(TwitchApi.GetLogger());
+            throw new TokenTypeException(Enum.GetName(TwitchToken.TokenType.App), Enum.GetName(this.Token.Type ?? TwitchToken.TokenType.Unknown)).Log(TwitchApi.GetLogger());
         }
     }
+
+    /// <summary>
+    /// Checks if either an app or user token is present. If it is a user token, also checks if the specified scopes are present.
+    /// </summary>
+    /// <param name="scopes">The scopes to check for.</param>
+    /// <exception cref="InvalidOperationException"><see cref="Token"/> is <see langword="null"/>; <see cref="TwitchToken.OAuth"/> is <see langword="null"/>, empty, or whitespace.</exception>
+    /// <exception cref="TokenTypeException"><see cref="TwitchToken.Type"/> is not <see cref="TwitchToken.TokenType.App"/> or <see cref="TwitchToken.TokenType.User"/>.</exception>
+    /// <exception cref="TwitchScopeMissingException"><see cref="TwitchToken.Type"/> is <see cref="TwitchToken.TokenType.User"/> and <see cref="TwitchToken.Scopes"/> does not contain all of the scopes defined in <paramref name="scopes"/>.</exception>
+    /// <remarks>
+    /// If <see cref="TwitchToken.Scopes"/> is <see langword="null"/>, then checking of <paramref name="scopes"/> is skipped.
+    /// </remarks>
+    public void RequireUserOrAppToken(params Scope?[] scopes)
+    {
+        if (this.Token is null)
+        {
+            throw new InvalidOperationException(nameof(this.Token) + " is null.").Log(TwitchApi.GetLogger());
+        }
+
+        if (string.IsNullOrWhiteSpace(this.Token.OAuth))
+        {
+            throw new InvalidOperationException(nameof(this.Token.OAuth) + " is null, empty, or whitespace.").Log(TwitchApi.GetLogger());
+        }
+
+        if (this.Token.Type is not TwitchToken.TokenType.App or TwitchToken.TokenType.User)
+        {
+            throw new TokenTypeException(Enum.GetName(TwitchToken.TokenType.App) + " or " + Enum.GetName(TwitchToken.TokenType.User), Enum.GetName(this.Token.Type ?? TwitchToken.TokenType.Unknown)).Log(TwitchApi.GetLogger());
+        }
+
+        if (this.Token.Type is TwitchToken.TokenType.User)
+        {
+            (_, Scope?[] Missing) = this.Token.CheckScopes(scopes);
+
+            if (Missing.Length > 0)
+            {
+                throw new TwitchScopeMissingException(Missing).Log(TwitchApi.GetLogger());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks if a user token is present and contains the specified scopes.
+    /// </summary>
+    /// <param name="scopes">The scopes to check for.</param>
+    /// <exception cref="InvalidOperationException"><see cref="Token"/> is <see langword="null"/>; <see cref="TwitchToken.OAuth"/> is <see langword="null"/>, empty, or whitespace.</exception>
+    /// <exception cref="TokenTypeException"><see cref="TwitchToken.Type"/> is not <see cref="TwitchToken.TokenType.User"/>.</exception>
+    /// <exception cref="TwitchScopeMissingException"><see cref="TwitchToken.Scopes"/> does not contain all of the scopes defined in <paramref name="scopes"/>.</exception>
+    /// <remarks>
+    /// If <see cref="TwitchToken.Scopes"/> is <see langword="null"/>, then checking of <paramref name="scopes"/> is skipped.
+    /// </remarks>
+    public void RequireUserToken(params Scope?[] scopes)
+    {
+        if (this.Token is null)
+        {
+            throw new InvalidOperationException(nameof(this.Token) + " is null.").Log(TwitchApi.GetLogger());
+        }
+
+        if (string.IsNullOrWhiteSpace(this.Token.OAuth))
+        {
+            throw new InvalidOperationException(nameof(this.Token.OAuth) + " is null, empty, or whitespace.").Log(TwitchApi.GetLogger());
+        }
+
+        if (this.Token.Type is not TwitchToken.TokenType.User)
+        {
+            throw new TokenTypeException(Enum.GetName(TwitchToken.TokenType.User), Enum.GetName(this.Token.Type ?? TwitchToken.TokenType.Unknown)).Log(TwitchApi.GetLogger());
+        }
+
+        (_, Scope?[] Missing) = this.Token.CheckScopes(scopes);
+
+        if (Missing.Length > 0)
+        {
+            throw new TwitchScopeMissingException(Missing).Log(TwitchApi.GetLogger());
+        }
+    }
+
+    #endregion Public Methods
+
+    #region Internal Fields
+
+    /// <summary>
+    /// Used as a locking object by <see cref="TwitchApi.PerformHttpRequest(HttpMethod, Uri, TwitchSession, HttpContent?, bool)"/> to prevent multiple simultaneous or back-to-back OAuth Refreshes.
+    /// </summary>
+    internal Mutex _refreshLock = new();
+
+    #endregion Internal Fields
+
+    #region Private Fields
+
+    /// <summary>
+    /// Backer for <see cref="Token"/>.
+    /// </summary>
+    private TwitchToken? _token;
+
+    #endregion Private Fields
 }
